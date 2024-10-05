411380ee93073945528a6d652a1116f6db0de475
lapiedranumerodos <maria.orgniani@estudiante.imagecampus.edu.ar>
2024-10-05 19:14:20 -0300

    add tower area UI on pointer enter

---
+++ Assets/Scripts/UI/TowerAreaUI.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TowerAreaUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image attackRadius;
+    [SerializeField] private float animationSpeed = 1f;
+
+    [SerializeField] private float minAlpha = 0f;
+
+    private Color startColor;
+    private Color endColor = new Color(1f, 1f, 1f, 1f);
+
+    private Coroutine currentAnimation;
+
+    private void Awake()
+    {
+        startColor = attackRadius.color;
+        endColor = new Color(startColor.r, startColor.g, startColor.b, minAlpha);
+        attackRadius.color = endColor;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (currentAnimation != null)
+            StopCoroutine(currentAnimation);
+
+        currentAnimation = StartCoroutine(AnimateAppearance(true));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (currentAnimation != null)
+            StopCoroutine(currentAnimation);
+
+        currentAnimation = StartCoroutine(AnimateAppearance(false));
+    }
+
+    private IEnumerator AnimateAppearance(bool fadeIn)
+    {
+        float t = 0f;
+        Color start = fadeIn ? endColor : startColor;
+        Color end = fadeIn ? startColor : endColor;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * animationSpeed;
+            Color currentColor = Color.Lerp(start, end, t);
+            attackRadius.color = currentColor;
+            yield return null;
+        }
+
+        currentAnimation = null;
+    }
+}
